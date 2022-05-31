7897d0bcd3311a6896ebdaa7105e51e1ebcbaad9
Keld <keldarch@amalia.anonaddy.com>
2022-05-30 20:47:51 -0400

    added separate class that extends Application to hold worker logic

---
+++ app/src/main/java/com/udacity/asteroidradar/App.kt
@@ -0,0 +1,56 @@
+package com.udacity.asteroidradar
+
+import android.app.Application
+import androidx.work.*
+import com.udacity.asteroidradar.workers.RefreshDataWork
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
+
+class App : Application(), Configuration.Provider {
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
+    override fun getWorkManagerConfiguration(): Configuration {
+        return if (BuildConfig.DEBUG) {
+            Configuration.Builder()
+                .setMinimumLoggingLevel(android.util.Log.DEBUG)
+                .build()
+        } else {
+            Configuration.Builder()
+                .setMinimumLoggingLevel(android.util.Log.ERROR)
+                .build()
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        if(BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+        delayedInit()
+    }
+
+    private fun delayedInit() = applicationScope.launch {
+        setupRecurringWork()
+    }
+
+    private fun setupRecurringWork() {
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresBatteryNotLow(true)
+            .setRequiresCharging(true)
+            .build()
+
+        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWork>(1, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(
+            RefreshDataWork.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest
+        )
+    }
+}
--- app/src/main/java/com/udacity/asteroidradar/MainActivity.kt
+++ app/src/main/java/com/udacity/asteroidradar/MainActivity.kt
@@ -12,34 +12,8 @@ import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
 
-    private val applicationScope = CoroutineScope(Dispatchers.Default)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Timber.plant(Timber.DebugTree())
-        delayedInit()
-    }
-
-    private fun delayedInit() = applicationScope.launch {
-        setupRecurringWork()
-    }
-
-    private fun setupRecurringWork() {
-        val constraints = Constraints.Builder()
-            .setRequiredNetworkType(NetworkType.UNMETERED)
-            .setRequiresBatteryNotLow(true)
-            .setRequiresCharging(true)
-            .build()
-
-        val repeatingRequest = PeriodicWorkRequestBuilder<RefreshDataWork>(1, TimeUnit.DAYS)
-            .setConstraints(constraints)
-            .build()
-
-        WorkManager.getInstance(applicationContext).enqueueUniquePeriodicWork(
-            RefreshDataWork.WORK_NAME,
-            ExistingPeriodicWorkPolicy.KEEP,
-            repeatingRequest
-        )
     }
 }
